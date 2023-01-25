a0ff8fa1b63b556357924a46e30b19ed3b1e04b0
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-01-24 23:11:00 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, watch, series } = require("gulp");
+
+//CSS Y SASS
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+//Imagenes
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif")
+
+function css(done) {
+    //compilar sass
+    //pasos:
+    //1. identificar archivos
+    //2. compilarlos
+    //3. guardar el css
+
+    src("src/scss/app.scss")
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest("build/css"));
+    done();
+}
+
+function imagenes(done) {
+    src("src/img/**/*")
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
+    return src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+}
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src("src/img/**/*.{png,jpg}").pipe(avif(opciones)).pipe(dest("build/img"));
+}
+
+function dev() {
+    watch("src/scss/**/*.scss", css);
+    watch("src/img/**/*", imagenes);
+    // watch("src/scss/app.scss", css);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+// exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
+exports.default = series(css, dev);
